da2ea4df8c4b6ef0ad9f4edf8f36d577dcf0b364
Jonathan Tung <jtung23@gmail.com>
2017-08-09 16:03:36 -0700

    css bootstrap stuff

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,10 @@
 
 $(document).ready(function() {
+	
 	var totalnumber = 0
-
-	var imgValue = $('<value>')
 	var randomNumbers = Math.floor(Math.random() * 9) + 1;
+	var win = $('#wins').html()
+	var lose = $('#lose').html()
 	
 	function generateRandomValue(min, max) {
 		min = Math.ceil(min);
@@ -14,28 +15,50 @@ $(document).ready(function() {
 		$('#wins').html(0)
 		$('#lose').html(0)
 	}
-	setWinLose()
-// Generates random value for each image //////////
-	$('#img1').attr('value', generateRandomValue(1, 12));
-	$('#img2').attr('value', generateRandomValue(1, 12));
-	$('#img3').attr('value', generateRandomValue(1, 12));
-	$('#img4').attr('value', generateRandomValue(1, 12));
 
-	$('#random-number').html(generateRandomValue(19, 120))
+	function resetAfterWinLose() {
+		$('#total-number').html(totalnumber)
+	}
+
+	function generateAllRandomValues() {
+		$('#img1').attr('value', generateRandomValue(1, 12));
+		$('#img2').attr('value', generateRandomValue(1, 12));
+		$('#img3').attr('value', generateRandomValue(1, 12));
+		$('#img4').attr('value', generateRandomValue(1, 12));
+
+		$('#random-number').html(generateRandomValue(19, 120));
+	}
+	
+	setWinLose();
+
+	resetAfterWinLose();
+	generateAllRandomValues();
+// Generates random value for each image //////////
+
+	// put values into array to not repeat.
 
 	// When image is clicked function //////////////
 	$('img').click(function() {
-		var result = $('#total-number').text()
-		var imageClickValue = parseInt($(this).attr('value'))
-		var resultInteger = parseInt(result)
-		var sum = imageClickValue + resultInteger
-
+		var result = $('#total-number').text();
+		var imageClickValue = parseInt($(this).attr('value'));
+		var resultInteger = parseInt(result);
+		var sum = imageClickValue + resultInteger;
 		$('#total-number').text(sum)
-		if ($('#total-number').text() > $('#random-number').text()) {
-			console.log('win')
-			// $('#wins').attr('id', ++)
-		} else if ($('#total-number').text() === $('#random-number').text()) {
-			console.log('l')
+
+		var totalAsInt = parseInt($('#total-number').text())
+		var randomAsInt = parseInt($('#random-number').text())
+
+		if (totalAsInt > randomAsInt) {
+				console.log('lose');
+				$('#lose').html(parseInt($('#lose').html()) + 1);
+				resetAfterWinLose();
+				generateAllRandomValues();
+			}
+		else if (totalAsInt === randomAsInt) {
+			console.log('win');
+			$('#wins').html(parseInt($('#wins').html()) + 1);
+			resetAfterWinLose();
+			generateAllRandomValues();
 		}
 	})
 
